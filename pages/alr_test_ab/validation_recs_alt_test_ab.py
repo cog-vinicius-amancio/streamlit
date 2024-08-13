a55017f44467a56d03f57f4191a0e8d0fa7c945a
Pedro Dicati <pedro.dicati@cognitivo.ai>
2024-08-13 09:32:43 -0300

    feat: add view dataframe and validation product id

--- pages/alr_test_ab/validation_recs_alt_test_ab.py
+++ pages/alr_test_ab/validation_recs_alt_test_ab.py
@@ -19,28 +19,32 @@ def model_alr():
 
     product = st.text_input("Digite o código do produto (sem cor):").upper().strip()
 
-    recs_filtered = data.loc[data["productId"].str.contains(product, case=False)][
-        "cd_prods_rec"
-    ].unique()[:12]
+    recs_filtered = (
+        data.loc[data["productId"].str.contains(product, case=False)]
+        .sort_values("rank", ascending=True)["cd_prods_rec"]
+        .unique()
+    )
 
-    if st.button("Buscar"):
+    st.dataframe(data.loc[data["productId"].str.contains(product, case=False)])
+
+    if st.button("Buscar Imagens e Informações"):
         if recs_filtered.size > 0:
             st.write("Produtos recomendados:")
             for rec in recs_filtered:
                 response = request_api_vtex(rec)
                 if response:
-                    st.write(f"**Nome**: {response['products'][0]['productName']}")
-                    st.image(
-                        response["products"][0]["items"][0]["images"][0]["imageUrl"],
-                        width=200,
-                    )
-                    st.write(
-                        f"**Descrição**: {response['products'][0]['description']}"
-                    )
-                    st.write(
-                        f"**Preço**: R$ {response['products'][0]['items'][0]['sellers'][0]['commertialOffer']['Price']}"
-                    )
-                    st.write("---" * 50)
+                    for product in response["products"]:
+                        if product["productReference"][:10] == rec:
+                            st.write(f"**Nome**: {product['productName']}")
+                            st.image(
+                                product["items"][0]["images"][0]["imageUrl"],
+                                width=200,
+                            )
+                            st.write(f"**Descrição**: {product['description']}")
+                            st.write(
+                                f"**Preço**: R$ {product['items'][0]['sellers'][0]['commertialOffer']['Price']}"
+                            )
+                            st.write("---" * 50)
                 else:
                     st.write("Erro ao processar requisição")
         else:
