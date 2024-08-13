87f976b3e9f9e2e11fac18e18fc7d504ad2655c6
Pedro Dicati <pedro.dicati@cognitivo.ai>
2024-08-13 10:19:14 -0300

    style: update page config

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import streamlit as st
 from pages.fathes_days_page.fathers_days import fathers_day
 from pages.alr_test_ab.validation_recs_alt_test_ab import model_alr
 
+st.set_page_config(
+    page_title="Aramis Testing Pages",
+    page_icon=":tshirt:",
+    layout="centered"
+)
+
 PAGES = {
     "Dia dos Pais": fathers_day,
     "Modelo ALR - Teste A/B": model_alr,
