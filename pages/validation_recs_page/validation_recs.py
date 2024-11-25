32e47921348768cc45147783f5602fa0f52ddb55
cog-vinicius-amancio <vinicius.amancio@cognitivo.ai>
2024-11-25 07:51:24 -0400

    feat:  added iteration over filtered_df and displaying recs lists

--- pages/validation_recs_page/validation_recs.py
+++ pages/validation_recs_page/validation_recs.py
@@ -9,31 +9,38 @@ PAGE_URL = 'pages/validation_recs_page/'
 def string_to_list(string: str) -> List:
     return json.loads(string)
 
+def request_images(row):
+    st.write(f"Recomendações para o produto {row['cd_prod_cor']}:")
+    st.write(string_to_list(row['recs']))
+
 def validation_recs():
 
-    st.title("Selecione Linhas do CSV")
+    st.title("Página de validação de recomendações")
     df = pd.read_csv(PAGE_URL+'rec.csv')
 
-    st.write("Dados carregados do CSV:")
-    event = st.dataframe(
-        df,
-        use_container_width=True,
-        on_select="rerun",
-        hide_index=True,
-        selection_mode="multi-row"
-    )
+    if not df.empty:
+        st.write("Selecione Linhas do CSV contendo os produtos, para obter as imagens das recomendações:")
+        event = st.dataframe(
+            df,
+            use_container_width=True,
+            on_select="rerun",
+            hide_index=True,
+            selection_mode="multi-row"
+        )
 
-    st.write("Produtos selecionados:")
-    selected_rows = event.selection.rows
-    filtered_df = df.iloc[selected_rows]
+        st.write("Produtos selecionados:")
+        selected_rows = event.selection.rows
+        filtered_df = df.iloc[selected_rows]
 
-    st.dataframe(
-        filtered_df,
-        use_container_width=True,
-        hide_index=True
-    )
+        st.dataframe(
+            filtered_df,
+            use_container_width=True,
+            hide_index=True
+        )
 
-    if st.button("Buscar recomendações"):
-        with st.spinner("Buscando recomendações..."):
-            for cd_prod_cor in filtered_df:
-                st.write(f"{cd_prod_cor}:")
+        if st.button("Buscar recomendações"):
+            with st.spinner("Buscando recomendações..."):
+                if not filtered_df.empty:
+                    filtered_df.apply(request_images, axis=1)
+                else:
+                    st.write("Selecione algum produto.")
