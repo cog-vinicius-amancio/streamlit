360578cac0c700d47d05e46e1ea9b45e399b9907
Pedro Dicati <66738952+pedrodicati@users.noreply.github.com>
2024-12-12 13:03:29 +0000

    feat: add api in validation rec page

--- pages/validation_recs_page/validation_recs.py
+++ pages/validation_recs_page/validation_recs.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from pages.fathes_days_page.fathers_days import request_api_vtex
 from typing import List
+from pages.test_api.test_api_recommendations import BASE_URL, MAPPER_ENDPOINTS
+from pages.test_api.test_api_recommendations import request_api, process_json
 import json
 import requests as req
 
@@ -49,6 +51,11 @@ def request_images(row,num_recs):
 def validation_recs():
 
     st.title("Página de validação de recomendações")
+
+    api_or_csv = st.selectbox(
+        "API ou CSV", ["API", "CSV"], index=0
+    )
+
     df = pd.read_csv(PAGE_URL+'recs.csv')
 
     if not df.empty:
@@ -61,21 +68,56 @@ def validation_recs():
             selection_mode="multi-row"
         )
 
-        st.write("Produtos selecionados:")
-        selected_rows = event.selection.rows
-        filtered_df = df.iloc[selected_rows]
+        if api_or_csv == "API":
+            key = st.selectbox(
+                "Selecione o produto para pesquisar",
+                df["cd_prod_cor"].unique(),
+                index=None
+            )
 
-        st.dataframe(
-            filtered_df,
-            use_container_width=True,
-            hide_index=True
-        )
-
-        num_recs = st.number_input('Número de recomendações que serão exibidas para cada produto selecionado:', min_value=1, value=5, step=1)
-
-        if st.button("Buscar recomendações"):
-            with st.spinner("Buscando recomendações..."):
-                if not filtered_df.empty:
-                    filtered_df.apply(lambda row: request_images(row, num_recs), axis=1)
+            if key:
+                st.subheader(f"Imagem do produto {key}:")
+                image_url = request_image(key)
+                if image_url:
+                    st.image(image_url, width=300)
                 else:
-                    st.write("Selecione algum produto.")
+                    st.write("Imagem não encontrada.")
+                st.subheader(f"As imagens das recomendações para o produto {key}:")
+
+                with st.spinner("Buscando recomendações..."):
+                    response, success = request_api("Produto Indisponível", key)
+
+                    if success:
+                        products = process_json(response)
+
+                        for product in products:
+                            st.write(f"**ID:** {product['id']}")
+                            st.write(f"**Nome:** {product['name']}")
+                            st.image(product["image"], width=300)
+                            st.write(f"**Link:** {product['link']}")
+                            st.write(f"**Preço de:** R$ {product['price_per']}")
+                            st.write(f"**Preço por:** R$ {product['price_of']}")
+                            st.write("---")
+                    else:
+                        st.error(response)
+            else:
+                st.warning("Selecione um produto para continuar")
+        else:
+            st.write("Produtos selecionados:")
+            selected_rows = event.selection.rows
+            filtered_df = df.iloc[selected_rows]
+
+            st.dataframe(
+                filtered_df,
+                use_container_width=True,
+                hide_index=True
+            )
+
+            num_recs = st.number_input('Número de recomendações que serão exibidas para cada produto selecionado:', min_value=1, value=5, step=1)
+
+            if st.button("Buscar recomendações"):
+                with st.spinner("Buscando recomendações..."):
+                    if not filtered_df.empty:
+                        filtered_df.apply(lambda row: request_images(row, num_recs), axis=1)
+                    else:
+                        st.write("Selecione algum produto.")
